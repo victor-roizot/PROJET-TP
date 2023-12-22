395ad1bdb93282d8062b08f1c0a6e39eecbf2066
demmo git <roizotvictor@gmail.com>
2023-12-22 13:44:26 +0100

    REGISTER

--- models/usersModel.php
+++ models/usersModel.php
@@ -3,17 +3,20 @@ class Users
 {
     private $pdo;
     public int $id;
-    public string $username;
+    public string $lastname;
+    public string $firstname;
+    public string $address;
+    public int $zipCode;
+    public string $city;
+    public string $phoneNumber;
     public string $email;
     public string $password;
-    public string $birthdate;
-    public string $registerDate;
-    public int $id_usersRoles;
+    public int $id_usersroles;
 
     public function __construct()
     {
         try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=corrections_pdo_la-manu-post;charset=utf8', 'lk85m_admin', 'RFjM6KaM$fDaqX9M');
+            $this->pdo = new PDO('mysql:host=localhost;dbname=projet_tp;charset=utf8', 'root', '');
         } catch(PDOException $e){
             header('Location: /index.php');
         }
@@ -25,7 +28,7 @@ class Users
      * @return bool
      */
     public function checkIfExistsByEmail() {
-        $sql = 'SELECT COUNT(`email`) FROM `pab7o_users` WHERE `email` = :email';
+        $sql = 'SELECT COUNT(`email`) FROM `HuBX02_users` WHERE `email` = :email';
         $req = $this->pdo->prepare($sql);
         $req->bindValue(':email', $this->email, PDO::PARAM_STR);
         $req->execute();
@@ -34,33 +37,43 @@ class Users
 
         /**
      * Vérifie si un utilisateur existe dans la base de données avec le nom d'utilisateur
-     * @param string $username Le nom d'utilisateur
+     * @param string $lastname Le nom d'utilisateur
      * @return bool
      */
-    public function checkIfExistsByUsername() {
-        $sql = 'SELECT COUNT(`username`) FROM `pab7o_users` WHERE `username` = :username';
+    public function checkIfExistsByLastname() {
+        $sql = 'SELECT COUNT(`lastname`) FROM `HuBX02_users` WHERE `lastname` = :lastname';
         $req = $this->pdo->prepare($sql);
-        $req->bindValue(':username', $this->username, PDO::PARAM_STR);
+        $req->bindValue(':lastname', $this->lastname, PDO::PARAM_STR);
         $req->execute();
         return $req->fetch(PDO::FETCH_COLUMN);
     }
 
     /**
      * Ajoute un utilisateur dans la base de données
-     * @param string $username Le nom d'utilisateur
-     * @param string $email L'adresse email
-     * @param string $password Le mot de passe hashé
-     * @param string $birthdate La date de naissance au format YYYY-MM-DD
+     * @param string $lastname Le nom d'utilisateur
+     * @param string $firstname Le prénom d'utilisateur
+     * @param string $address L'adresse de l'utilisateur
+     * @param int $zipCode Le code postal de l'utilisateur
+     * @param string $city La ville de l'utilisateur
+     * @param string $phoneNumber Le numéro de l'utilisateur
+     * @param string $email L'adresse email de l'utilisateur
+     * @param string $password Le mot de passe hashé de l'utilisateur
+     * @param string $id_usersroles Role de l'utilisateur
      * @return bool
      */
-    public function create() {
-        $sql = 'INSERT INTO `pab7o_users` (`username`,`email`,`password`,`birthdate`,`registerDate`,`id_usersRoles`) 
-        VALUES (:username,:email,:password,:birthdate, NOW(), 1)';
+    public function createUser() {
+        $sql = 'INSERT INTO `HuBX02_users` (`lastname`,`firstname`,`address`,`zipCode`,`city`,`phoneNumber`,`email`,`password`,`id_usersroles`) 
+        VALUES (:lastname,:firstname,:address,:zipCode,:city,:phoneNumber,:email,:password,:id_usersroles,)';
         $req = $this->pdo->prepare($sql);
-        $req->bindValue(':username', $this->username, PDO::PARAM_STR);
+        $req->bindValue(':lastname', $this->lastname, PDO::PARAM_STR);
+        $req->bindValue(':firstname', $this->firstname, PDO::PARAM_STR);
+        $req->bindValue(':address', $this->address, PDO::PARAM_STR);
+        $req->bindValue(':zipCode', $this->zipCode, PDO::PARAM_INT);
+        $req->bindValue(':city', $this->city, PDO::PARAM_STR);
+        $req->bindValue(':phoneNumber', $this->phoneNumber, PDO::PARAM_STR);
         $req->bindValue(':email', $this->email, PDO::PARAM_STR);
         $req->bindValue(':password', $this->password, PDO::PARAM_STR);
-        $req->bindValue(':birthdate', $this->birthdate, PDO::PARAM_STR);
+        $req->bindValue(':id_usersroles', $this->id_usersroles, PDO::PARAM_INT);
         return $req->execute();
     }
 
