db9175cd1903a2deb3e527b97fcbd9c0e5e097f6
demmo git <roizotvictor@gmail.com>
2024-01-15 16:54:42 +0100

    AJOUT UPDATE

--- models/usersModel.php
+++ models/usersModel.php
@@ -82,31 +82,25 @@ class Users
      *VALUES ('lastname','firstname','50 rue de paris',75000,'PARIS','06 00 00 00 00','test@gmail.com','Pizza123!', 1);
      */
 
-    public function getPassword()
-    {
-        $sql = 'SELECT `password` FROM `hubx02_users` WHERE `email` = :email';
-        $req = $this->pdo->prepare($sql);
-        $req->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $req->execute();
-        return $req->fetch(PDO::FETCH_COLUMN);
-    }
-
-    public function getInfosByEmail()
-    {
-        $sql = 'SELECT `id`, `lastname`, `firstname`, `address`, `id_usersroles` FROM `hubx02_users` WHERE `email` = :email';
-        $req = $this->pdo->prepare($sql);
-        $req->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $req->execute();
-        return $req->fetch(PDO::FETCH_ASSOC);
-    }
-    /** requete test dans PHP MySQL
-     * SELECT `id`, `lastname`, `firstname`, `id_usersroles` FROM `hubx02_users` WHERE `email` = 'jean.dupont@gmail.com';
+    /**
+     * Supprime un utilisateur selon son id
+     * @param int $id L'id de l'utilisateur à supprimer
+     * @return bool
      */
-
     public function delete()
     {
+        $sql =  'DELETE FROM `hubx02_users` WHERE `id` = :id';
+        $req = $this->pdo->prepare($sql);
+        $req->bindValue(':id', $this->id, PDO::PARAM_INT);
+        return $req->execute();
     }
 
+
+    /**
+     * Récupère les informations d'un utilisateur dans la base de données avec son id
+     * @param int $id L'id de l'utilisateur qui nous permettra de récupérer ses informations
+     * @return object Les informations de l'utilisateur
+     */
     public function getById()
     {
         $sql = 'SELECT `lastname`, `firstname`, `email`, `phoneNumber`, `name` AS `roleName`
@@ -125,11 +119,81 @@ class Users
      * WHERE`hubx02_users`.`id` = 1;
      */
 
-    public function getList()
+    
+    /**
+     * Récupère les informations d'un utilisateur dans la base de données avec son adresse email
+     * @param string $email L'adresse email de l'utilisateur qui nous permettra de récupérer ses informations
+     * @return array Les informations de l'utilisateur
+     */
+    public function getInfosByEmail()
     {
+        $sql = 'SELECT `id`, `lastname`, `firstname`, `address`, `id_usersroles` FROM `hubx02_users` WHERE `email` = :email';
+        $req = $this->pdo->prepare($sql);
+        $req->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $req->execute();
+        return $req->fetch(PDO::FETCH_ASSOC);
+    }
+    /** requete test dans PHP MySQL
+     * SELECT `id`, `lastname`, `firstname`, `id_usersroles` FROM `hubx02_users` WHERE `email` = 'jean.dupont@gmail.com';
+     */
+
+    
+    /**
+     * Récupère le mot de passe hashé d'un utilisateur dans la base de données. 
+     * On l'utilisera pour vérifier avec la fonction password_verify que le mot de passe saisi par l'utilisateur correspond bien à celui stocké dans la base de données.
+     * @param string $email L'adresse email de l'utilisateur qui nous permettra de récupérer son mot de passe
+     * @return string Le mot de passe hashé
+     */
+    public function getPassword()
+    {
+        $sql = 'SELECT `password` FROM `hubx02_users` WHERE `email` = :email';
+        $req = $this->pdo->prepare($sql);
+        $req->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $req->execute();
+        return $req->fetch(PDO::FETCH_COLUMN);
     }
 
-    public function update()
+        /**
+     * Met à jour le nom de l'utilisateur, l'adresse email et la date de naissance
+     * @param string $username Le nom de l'utilisateur
+     * @param string $email L'adresse email de l'utilisateur
+     * @param string $birthdate La date de naissance de l'utilisateur
+     * @param int $id L'id de l'utilisateur à modifier
+     * @return bool
+     */
+    public function updateAdress()
     {
+        $sql = 'UPDATE `hubx02_users` SET `lastname` = :lastname, `email` = :email, `birthdate` = :birthdate WHERE `id`= :id';
+        $req = $this->pdo->prepare($sql);
+        $req->bindValue(':lastname', $this->lastname, PDO::PARAM_STR);
+        $req->bindValue(':firstname', $this->firstname, PDO::PARAM_STR);
+        $req->bindValue(':address', $this->address, PDO::PARAM_STR);
+        $req->bindValue(':zipCode', $this->zipCode, PDO::PARAM_INT);
+        $req->bindValue(':city', $this->city, PDO::PARAM_STR);
+        $req->bindValue(':phoneNumber', $this->phoneNumber, PDO::PARAM_STR);
+        $req->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $req->bindValue(':password', $this->password, PDO::PARAM_STR);
+        return $req->execute();
+    }
+
+    {
+        $sql = 'INSERT INTO `hubx02_users` (`lastname`,`firstname`,`address`,`zipCode`,`city`,`phoneNumber`,`email`,`password`,`id_usersroles`) 
+        VALUES (:lastname,:firstname,:address,:zipCode,:city,:phoneNumber,:email,:password,1)';
+        }
+
+
+        /**
+     * Met à jour le mot de passe de l'utilisateur
+     * @param string $password Le mot de passe de l'utilisateur
+     * @param int $id L'id de l'utilisateur à modifier
+     * @return bool
+     */
+    public function updatePassword()
+    {
+        $sql = 'UPDATE `hubx02_users` SET `password` = :password WHERE `id`= :id';
+        $req = $this->pdo->prepare($sql);
+        $req->bindValue(':password', $this->password, PDO::PARAM_STR);
+        $req->bindValue(':id', $this->id, PDO::PARAM_INT);
+        return $req->execute();
     }
 }
