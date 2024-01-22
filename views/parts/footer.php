b99d4ada2faa9d8d3b882cfb59d062eee4afd523
demmo git <roizotvictor@gmail.com>
2024-01-22 18:20:50 +0100

    MODIF CSS NAVBAR

--- views/parts/footer.php
+++ views/parts/footer.php
@@ -1,7 +1,8 @@
 </main>
 <footer>
 
-    <h3>Blackwood - Copyright 2024</h3>
+    <h3><a class="lien" href="/accueil">Blackwood</a></h3>
+    <p>Copyright 2024</p>
 </footer>
 <script src="../../assets/js/script.js"></script>
 </body>
--- views/parts/header.php
+++ views/parts/header.php
@@ -16,21 +16,21 @@
 <body>
     <nav>
         <div>
-            <a href="#"><img id="logo" src="../../assets/img/lambda half life.jpg" alt="Logo"></a>
+            <a href="/accueil"><img id="logo" src="../../assets/img/lambda half life.jpg" alt="Logo"></a>
             <h1 id="name"> Blackwook</h1>
         </div>
         <ul>
-            <li><a href="/">Accueil</a></li>
+            <li><a class="lien" href="/accueil">Accueil</a></li>
             <?php if (empty($_SESSION['user'])) { ?>
-                <li><a href="/inscription">inscription</a></li>
-                <li><a href="/connexion">Connexion</a></li>
+                <li><a class="lien" href="/inscription">inscription</a></li>
+                <li><a class="lien" href="/connexion">Connexion</a></li>
             <?php } else { ?>
-                <li><a href="/mon-compte"><?= $_SESSION['user']['lastname']
+                <li><a class="lien" href="/mon-compte"><?= $_SESSION['user']['lastname']
                                             ?></a></li>
                 <?php if ($_SESSION['user']['id_usersroles'] == 255) { ?>
-                    <li><a href="/dashboard">Admin</a></li>
+                    <li><a class="lien" href="/dashboard">Admin</a></li>
                 <?php } ?>
-                <li><a href="/deconnexion">Déconnexion</a></li>
+                <li><a class="lien" href="/deconnexion">Déconnexion</a></li>
             <?php } ?>
         </ul>
     </nav>
