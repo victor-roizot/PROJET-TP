819df4d23bfe91db4c0a2d34ae8e5167aa6a5b9d
demmo git <roizotvictor@gmail.com>
2024-02-14 16:57:37 +0100

    ADD MENU BURGER

--- views/parts/header.php
+++ views/parts/header.php
@@ -24,7 +24,8 @@
 <body>
     <nav>
         <a href="/accueil"><img id="logo" src="../../assets/img/logo_cabin_escapes.png" alt="Logo"></a>
-        <ul class="navbar">
+        
+        <ul class="navbar-desktop">
             <li><a href="/accueil" class=lien>Accueil</a></li>
             <li><a href="/list-cabane" class=lien>Cabanes</a></li>
             <?php if (empty($_SESSION['user'])) { ?>
@@ -39,5 +40,28 @@
                 <li><a href="/deconnexion" class=lien>Déconnexion</a></li>
             <?php } ?>
         </ul>
+
+        <div class="navbar-mobile">
+            <i class="fas fa-bars"></i>
+            <div class="modal">
+                <ul class="navbar-mobile-list">
+                    <li><a href="/accueil" class=lien>Accueil</a></li>
+                    <li><a href="/list-cabane" class=lien>Cabanes</a></li>
+                    <?php if (empty($_SESSION['user'])) { ?>
+                        <li><a href="/inscription" class=lien>Inscription</a></li>
+                        <li><a href="/connexion" class=lien>Connexion</a></li>
+                    <?php } else { ?>
+                        <li><a href="/mon-compte" class="lien"><?= $_SESSION['user']['lastname'] ?></a></li>
+                        <!-- l'utilisateur aura accès à dashboard  uniquement s'il est administrateur -->
+                        <?php if ($_SESSION['user']['id_usersRoles'] == 255) { ?>
+                            <li><a href="/dashboard" class=lien>Admin</a></li>
+                        <?php } ?>
+                        <li><a href="/deconnexion" class=lien>Déconnexion</a></li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+
+
     </nav>
     <main>
