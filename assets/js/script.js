412b01294017a62d730b6cd8fc47c086b3a86a2f
demmo git <roizotvictor@gmail.com>
2024-02-15 10:32:29 +0100

    CSS MENU BURGER

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
+/* MODAL BURGER */
+
 // Je sélectionne et je stocke
-// l'icône burger
 const icone = document.querySelector('.navbar-mobile i'); 
-console.log(icone); 
 // la DIV modal
 const modal = document.querySelector('.modal'); 
-console.log(modal); 
 
 icone.addEventListener('click', function(){
-    console.log("icone cliquée"); 
     modal.classList.toggle('change-modal'); 
     icone.classList.toggle('fa-times'); 
 }); 
 
 /* MODAL DELETE */
+
 openModalBtn.addEventListener("click", () => {
     modalContainer.style.display = "flex"
 })
