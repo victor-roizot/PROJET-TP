10fdbbf23ccb7e9c7ef52e918a4ad94ffe9d1876
demmo git <roizotvictor@gmail.com>
2024-02-16 15:43:37 +0100

    UPDATE CSS

--- views/users/admin.php
+++ views/users/admin.php
@@ -1,5 +1,9 @@
 <!-- L'utilisateur  administrateur pourra ajouter, modifier ou supprimer des items de réservation par ce lien qui l'enverra vers addItem -->
 <section>
-    <h1>Tableau de bord - Dashboard</h1>
-    <a class=lien  href="/ajout-item">Ajouter une cabane</a>
+    <div class="viewContainer">
+        <div class="view">
+            <h1>Tableau de bord - Dashboard</h1>
+            <a class=lienForm href="/ajout-item">Ajouter une cabane</a>
+        </div>
+    </div>
 </section>
